a6499393eb72a7789a932e7b19900eec34935543
keviny22 <kevin_young@intuit.com>
2013-04-16 14:42:38 -0700

    cookbook_structure

---
+++ README.md
@@ -0,0 +1,13 @@
+# new_relic-cookbook cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Intuit, Inc. (<@intuit.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "new_relic-cookbook"
+maintainer       "Intuit, Inc."
+maintainer_email "@intuit.com"
+license          "Apache 2.0"
+description      "Installs/Configures new_relic-cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
