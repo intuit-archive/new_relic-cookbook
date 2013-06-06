1bab217f71b59c2c05c8208f9081fbb7b62aaea9
Tommy Bishop <bishop.thomas@gmail.com>
2013-06-06 14:01:04 -0700

    clean up app agent template var

--- recipes/java_agent.rb
+++ recipes/java_agent.rb
@@ -1,6 +1,6 @@
 template node['new_relic']['app_agent']['config_file'] do
   source 'java_agent_newrelic.yml.erb'
-  variables :config_var  => node['new_relic']['app_agent'],
+  variables :config      => node['new_relic']['app_agent'],
             :license_key => node['new_relic']['license_key'],
             :proxy       => node['new_relic']['proxy']
 end
