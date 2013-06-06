604e4e558d2e62c09b6e003dae7b452d88f231fd
Tommy Bishop <bishop.thomas@gmail.com>
2013-06-06 13:59:54 -0700

    rename ruby agent template to mimic java one

--- recipes/ruby_agent.rb
+++ recipes/ruby_agent.rb
@@ -1,5 +1,5 @@
 template node['new_relic']['app_agent']['config_file'] do
-  source 'newrelic.yml.erb'
+  source 'ruby_agent_newrelic.yml.erb'
   variables :config_var => node['new_relic']['app_agent'],
             :license_key => node['new_relic']['license_key'],
             :proxy       => node['new_relic']['proxy']
