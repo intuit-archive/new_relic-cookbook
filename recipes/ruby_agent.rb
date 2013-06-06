4ce5fb2a5c01c34eccdf8f16e7e723706f7ea990
Tommy Bishop <bishop.thomas@gmail.com>
2013-06-06 13:58:09 -0700

    update ruby agent recipe to include new proxy info

--- recipes/ruby_agent.rb
+++ recipes/ruby_agent.rb
@@ -1,5 +1,6 @@
 template node['new_relic']['app_agent']['config_file'] do
   source 'newrelic.yml.erb'
   variables :config_var => node['new_relic']['app_agent'],
-            :license_key => node['new_relic']['license_key']
+            :license_key => node['new_relic']['license_key'],
+            :proxy       => node['new_relic']['proxy']
 end
