4a9c2fcce8e25cd4336ac8d5d4099f1ce13c2800
Tommy Bishop <bishop.thomas@gmail.com>
2013-06-11 09:40:06 -0700

    first pass at proxy support for java deployment marker

--- resources/java_deployment_record.rb
+++ resources/java_deployment_record.rb
@@ -5,6 +5,9 @@ default_action :create
 attribute :app_name,     :kind_of => String
 attribute :command_path, :kind_of => String, :required => true
 attribute :environment,  :kind_of => String
+attribute :proxy         :kind_of => Boolean, :default => false
+attribute :proxy_host    :kind_of => String
+attribute :proxy_port    :kind_of => String
 attribute :revision,     :kind_of => String
 attribute :user,         :kind_of => String, :default => ENV['SUDO_USER'] ||
                                                          ENV['USER'] ||
