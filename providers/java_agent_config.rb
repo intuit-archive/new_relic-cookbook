230631a12e9e8ec4d5e610f9a373cb437583235f
capen <cbrinkley@gmail.com>
2013-07-12 14:52:59 -0700

    adding lwrp for java/rugy agent config

--- providers/java_agent_config.rb
+++ providers/java_agent_config.rb
@@ -1,4 +1,5 @@
-define :new_relic_java_agent_config do
+action :create do
   node.set['new_relic']['app_agent']['config_file'] = params[:name]
   include_recipe 'new_relic::java_agent'
+  new_resource.updated_by_last_action(true)
 end
